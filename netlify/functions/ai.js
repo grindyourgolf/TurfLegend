e3e87ed706f1b682c5e07cd576db3adaac2fa1e2
grindyourgolf <grindyourgolf@gmail.com>
2026-04-02 23:35:28 -0700

    Update ai.js

--- netlify/functions/ai.js
+++ netlify/functions/ai.js
@@ -1,47 +1,32 @@
-const https = require(“https”);
+var https = require(“https”);
 
 exports.handler = function(event, context, callback) {
-if (event.httpMethod !== “POST”) {
-return callback(null, { statusCode: 405, body: “Method Not Allowed” });
-}
-
 var body = event.body;
-var key = process.env.ANTHROPIC_API_KEY;
-
 var options = {
 hostname: “api.anthropic.com”,
 path: “/v1/messages”,
 method: “POST”,
 headers: {
 “Content-Type”: “application/json”,
-“x-api-key”: key,
+“x-api-key”: process.env.ANTHROPIC_API_KEY,
 “anthropic-version”: “2023-06-01”,
 “Content-Length”: Buffer.byteLength(body)
 }
 };
-
 var req = https.request(options, function(res) {
 var data = “”;
-res.on(“data”, function(chunk) { data += chunk; });
+res.on(“data”, function(c) { data += c; });
 res.on(“end”, function() {
 callback(null, {
 statusCode: res.statusCode,
-headers: {
-“Content-Type”: “application/json”,
-“Access-Control-Allow-Origin”: “*”
-},
+headers: { “Content-Type”: “application/json”, “Access-Control-Allow-Origin”: “*” },
 body: data
 });
 });
 });
-
-req.on(“error”, function(err) {
-callback(null, {
-statusCode: 500,
-body: JSON.stringify({ error: err.message })
+req.on(“error”, function(e) {
+callback(null, { statusCode: 500, body: JSON.stringify({ error: e.message }) });
 });
-});
-
 req.write(body);
 req.end();
 };
